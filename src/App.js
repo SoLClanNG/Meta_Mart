8f77275bfef48b6847b2b5873b71f0d8cac3cfe3
KSwitch1303 <ebimieteimisongo@gmail.com>
2023-08-18 19:08:17 +0100

    master commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import logo from './logo.svg';
+import './App.css';
+//import App from './login';
+
+const change = () => {
+    [settext, ]
+}
+
+function app () {
+  return(
+    <div>
+        <h3>CREATE PROFILE</h3>
+        <h2 id='h2'></h2>
+        <input placeholder="Name" className="inbox1" />
+
+        <button onClick={console.log('ass')}>
+          submit
+        </button>
+        <p></p>
+        <button // onClick={clearDB()}
+        >
+          CLEAR DB
+        </button>
+        <button><a href="/uploadProduct.html">
+          DONE
+        </a>
+        </button>
+        <script></script>
+    </div>
+  
+)
+}
+
+export default app
---
+++ src/components/navbar.js
@@ -0,0 +1,10 @@
+import React from "react";
+export default function Btn() {
+  return(
+    <div>
+      <button >
+        button
+      </button>
+    </div>
+  );
+}
